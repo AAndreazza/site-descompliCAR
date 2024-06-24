649f7c3f0a550774ff89e9cdc9dbfbaf9729f558
AAndreazza <alanaandreazza26@hotmail.com>
2024-06-24 12:32:23 -0300

    teste 4

--- src/components/teste.js
+++ src/components/teste.js
@@ -5,7 +5,7 @@ const Formulario = () => {
 
         <div className='isolate px-6 -mt-10 mb-20 lg:px-8 font-montserrat'>
 
-            <form name="contact" netlify className='mx-auto mt-16 max-w-xl sm:mt-20 rounded-3xl p-8 ring-2 bg-black shadow-2xl'>
+            <form name="teste-descomplicar" method="POST" data-netlify="true" className='mx-auto mt-16 max-w-xl sm:mt-20 rounded-3xl p-8 ring-2 bg-black shadow-2xl'>
                 <input type='text' name='nome' id='nome' autocomplete='given-name' placeholder='Nome completo' className='mt-2.5 block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 bg-gray-100 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' />
                 <input type='email' name='email' id='email' autocomplete='email' placeholder='E-mail' className='mt-2.5 block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 bg-gray-100 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' />
                 <input type='tel' name='telefone' id='telefone' autocomplete='tel' placeholder='Celular com DDD' className='mt-2.5 block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 bg-gray-100 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' />
